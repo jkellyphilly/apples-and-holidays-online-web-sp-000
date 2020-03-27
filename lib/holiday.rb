184ce511077354ddd6cf9d0a5a6b30685b63a97f
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-27 18:29:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
     season_string = season.to_s.capitalize! << ":"
     puts season_string
     season_holiday_list.each do |holiday, holiday_supplies|
-      # split the new symbol into
+      # split the new symbol into an array with each word
       holiday_array = holiday.to_s.split("_")
+      #holiday_array.collect
       binding.pry
       holiday_string = "  " << holiday.to_s.capitalize << ": "
       holiday_supplies.each do |supply|

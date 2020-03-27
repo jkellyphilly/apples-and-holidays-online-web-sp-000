4655189d45e5ebdf4d56732f29e0403f0bd153e7
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-27 18:46:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   bbq_holidays = []
 
   # loop through each season
